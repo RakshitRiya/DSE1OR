d8b082ced0dafe6eca963f74937967ae717e7db3
Riya Rakshit <46257735+RakshitRiya@users.noreply.github.com>
2020-08-16 21:25:29 +0530

    Update determinant.cpp

--- Determinant/determinant.cpp
+++ Determinant/determinant.cpp
@@ -1,47 +1,46 @@
-//functions for determinant calculator
-#include "determinant.h"
-
-
-
-float determinant(float ** matrix, int size) {
-    float det = 0;
-    if(!matrix)
-        return 0;
-    if(size == 1) 
-	det = matrix[0][0];
-    else {
-        float ** sub_matrix = new float*[size-1];
-        for(int i = 0; i < size -1; ++i)
-            sub_matrix[i] = new float[size-1];
-        for(int i = 0; i < size; ++i) {
-            for(int k = 1; k < size; ++k) {
-                int b = 0;
-                for(int j = 0; j < size; ++j) {
-                    if(j == i)
-     	 	        continue;
-                    sub_matrix[k-1][b] = matrix[k][j];
- 		    ++b;
-                }
+#include<iostream>
+#include<math.h>
+using namespace std;
+int determinant( int matrix[10][10], int n) {
+   int det = 0;
+   int submatrix[10][10];
+   if (n==1)
+   return matrix[0][0];
+   else if (n == 2)
+   return ((matrix[0][0] * matrix[1][1]) - (matrix[1][0] * matrix[0][1]));
+   else {
+      for (int x = 0; x < n; x++) {
+         int subi = 0;
+         for (int i = 1; i < n; i++) {
+            int subj = 0;
+            for (int j = 0; j < n; j++) {
+               if (j == x)
+               continue;
+               submatrix[subi][subj] = matrix[i][j];
+               subj++;
             }
-            int sign;
-            if(i % 2 == 0) 
-                sign = 1;
-            else
-                sign = -1;
-            det += sign * matrix[0][i] * determinant(sub_matrix, size-1);
-        } 
-    }
-    return det;
-}
-
-
-void test_disp(float ** matrix, int size) {
-    for(int i = 0; i < size; ++i) {
-        cout << endl;
-        for(int k = 0; k < size; ++k) {
-            cout << matrix[i][k]; 
+            subi++;
          }
-    }
-    cout << endl;
+         det = det + (pow(-1, x) * matrix[0][x] * determinant( submatrix, n - 1 ));
+      }
+   }
+   return det;
 }
-
+int main() {
+   int n, i, j;
+   int matrix[10][10];
+   cout << "Enter the size of the matrix:\n";
+   cin >> n;
+   cout << "Enter the elements of the matrix:\n";
+   for (i = 0; i < n; i++)
+   for (j = 0; j < n; j++)
+   cin >> matrix[i][j];
+   cout<<"The entered matrix is:"<<endl;
+   for (i = 0; i < n; i++) {
+      for (j = 0; j < n; j++)
+      cout << matrix[i][j] <<" ";
+      cout<<endl;
+   }
+   cout<<"Determinant of the matrix is "<< determinant(matrix, n);
+   return 0;
+}
